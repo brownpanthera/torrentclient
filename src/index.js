75d1003648d4fedea74da167bb55f15587c4fd28
brownpanthera <alifaizan1983@gmail.com>
2025-03-30 22:30:19 +0530

    half way there with announce input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import fs from "node:fs";
 import bencode from "bencode";
-import dgram from "dgram"
-import { URL } from "url";
+import crypto from "crypto";
 
 import { connectionReq } from "./tracker.js";
 
-
-const torrent = bencode.decode(fs.readFileSync('./torrents/puppy.torrent'));
+const torrent = bencode.decode(fs.readFileSync("./torrents/spiderman.torrent"));
 
 // Convert Uint8Array to Buffer then String
-const announceURL = Buffer.from(torrent.announce).toString()
+// const decoder = new TextDecoder("utf-8")
+// const announceURL = decoder.decode(torrent.announce);
 
-console.log("Tracker URL", announceURL)
-connectionReq(announceURL)
+// dithing the buffer
+const announceURL = Buffer.from(torrent.announce).toString();
+
+export const infoHash = (() => {
+    const info = torrent.info;
+    const bencodeInfo = bencode.encode(info);
+    return crypto.createHash("sha1").update(bencodeInfo).digest(); // Buffer
+  })();
+
+export function torrentFileSize() {
+    return torrent.info.files
+      ? torrent.info.files.reduce((total, file) => total + file.length, 0)
+      : torrent.info.length;
+  }
+// console.log("infoHash", infoHash.toString("hex")); // Print hex format
+
+// console.dir(torrent)
+connectionReq(announceURL);
--- src/tracker.js
+++ src/tracker.js
@@ -1,23 +1,106 @@
 import dgram, { Socket } from "dgram"
 import crypto from "crypto"
 import { URL } from "url"
+import { infoHash, torrentFileSize } from "./index.js"
 
 export function connectionReq(announceURL){
     const randomNumber = crypto.randomBytes(4).readUInt32BE();
     const buff = Buffer.alloc(16)
-    buff.writeUInt32BE(0x417, 0);
-    buff.writeUint32BE(0x27101980, 4);
-    buff.writeUInt32BE(0, 8);
-    buff.writeUInt32BE(randomNumber, 12)
+    buff.writeUInt32BE(0x417, 0); // magic constant of 4bytes
+    buff.writeUint32BE(0x27101980, 4); // another magic constant of 4bytes
+    buff.writeUInt32BE(0, 8); // action ID
+    buff.writeUInt32BE(randomNumber, 12) // random transaction ID
 
     const url = new URL(announceURL)
 
     // sendin udp req to tracker
     const socket = dgram.createSocket('udp4')
-    socket.send(buff, 0, buff.length, url.port, url.host, ()=> {
+    socket.send(buff, 0, buff.length, url.port, url.hostname, ()=> {
         console.log(`sent connection req to ${url.hostname}, ${url.port}`)
-    })    
+    }) 
 
-    // extracting the res which comes same as req
-    socket.on
+    // console.log("sending buffer", buff.toString("hex"));
+
+    // extracting the res which is same as req
+    socket.on("error", (err)=>{
+        console.err("socet error ", err)
+    })
+
+
+
+    function validateResponse(condition, errorMessage) {
+        if (!condition) {
+            console.error(errorMessage);
+            process.exit(1);
+        }
+    }
+    
+    socket.on("message", (msg, rinfo) => {
+        console.log(rinfo)
+        const actionID = msg.readUInt32BE(0);
+        const transactionID = msg.readUInt32BE(4);
+        const connectionID = msg.readBigUInt64BE(8);
+
+        console.log(msg.length)
+    
+        validateResponse(!(msg.length < 16), "Error: Packet size is smaller than expected.");
+        validateResponse(transactionID === randomNumber, "Error: Transaction ID does not match the one that was generated.");
+        validateResponse(actionID === 0, "Unexpected Action ID, expected a connection response.");
+
+
+        console.table({ actionID, transactionID, connectionID });
+        console.log(infoHash)
+        console.log(torrentFileSize())
+        socket.close()
+
+        announceReq(announceURL, connectionID)
+    });
+    
+}
+
+
+function announceReq(announceURL, connectionID){
+    const announceTransactionID = crypto.randomBytes(4).readUint32BE();
+    const peerID = crypto.randomBytes(20) 
+    const requestBuff = Buffer.alloc(98)
+
+    requestBuff.writeBigUInt64BE(BigInt(connectionID), 0);
+    requestBuff.writeUInt32BE(1, 8)
+    requestBuff.writeUInt32BE(announceTransactionID, 12)
+    requestBuff.copy(infoHash, 16)
+    requestBuff.copy(peerID, 36)
+    requestBuff.writeBigUInt64BE(BigInt(0), 56)
+    requestBuff.writeBigUInt64BE(BigInt(torrentFileSize()), 64)
+    requestBuff.writeBigUInt64BE(BigInt(0), 48)
+    requestBuff.writeUint32BE(2, 80)
+    requestBuff.writeUInt32BE(0, 84)
+    requestBuff.writeUInt32BE(crypto.randomInt(0, 2 ** 32), 88)
+    requestBuff.writeInt32BE(-1, 92);
+    requestBuff.writeUInt16BE(6881, 96);
+
+    const client = dgram.createSocket("udp4")
+    const {hostname, port} = new URL(announceURL)
+
+    client.send(requestBuff, 0, requestBuff.length, port, hostname, (err)=> {
+        if(err) console.error("Error sending req", err)
+            else console.log("Announce Req send")
+        
+    })
+
+    client.on("message", (msg, rinfo) => {
+    const actionID = msg.readUInt32BE(0);
+    const transactionID = msg.readUInt32BE(4);
+    const interval = msg.readUInt32BE(8);
+    const leechers = msg.readUInt32BE(12);
+    const seeders = msg.readUInt32BE(16);
+
+    if(actionID !== 1){
+        console.error("unexpected action ID, expected announce res")
+        return
+    }
+
+    console.log(interval)
+    console.log(leechers)
+    console.log(seeders)
+    })
 }
